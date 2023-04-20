a272370698e3aea69b3a0d3fdc9792c726e28929
SupraiiS <40554747+Suprais135@users.noreply.github.com>
2023-04-20 03:26:45 -0300

    Add files via upload

---
+++ JavaScript-programando-na-linguagem-da-web/main.js
@@ -0,0 +1,39 @@
+function TocaSom (seletorAudio) {
+   const elemento = document.querySelector(seletorAudio);  
+
+
+if (elemento && elemento.localName === 'audio'){
+    elemento.play();
+}
+   else {
+    //alert('Elemento não encontrado');
+    console.log('Elemento não encontrado ou seletor invalido')
+   }
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+//para 
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;//Template string
+    
+    tecla.onclick = function () {
+    TocaSom(idAudio);
+   };
+
+   tecla.onkeydown = function (evento) {
+    
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+        tecla.classList.add('ativa');
+    }
+    
+}
+
+   tecla.onkeyup = function(){
+    tecla.classList.remove('ativa');
+   }
+
+}
