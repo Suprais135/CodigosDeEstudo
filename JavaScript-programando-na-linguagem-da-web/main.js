f3b05eca0b8439fbfd5f77f9cffecd2b0d26f838
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2024-02-12 16:43:12 -0300

    Corrigindo problema de tentar tocar o mesmo som mais de uma vez

--- JavaScript-programando-na-linguagem-da-web/main.js
+++ JavaScript-programando-na-linguagem-da-web/main.js
@@ -1,14 +1,17 @@
-function TocaSom (seletorAudio) {
-   const elemento = document.querySelector(seletorAudio);  
+function TocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-
-if (elemento && elemento.localName === 'audio'){
-    elemento.play();
-}
-   else {
-    //alert('Elemento não encontrado');
-    console.log('Elemento não encontrado ou seletor invalido')
-   }
+    if (elemento && elemento.localName === 'audio') {
+        if (elemento.play) {
+            elemento.pause();
+            elemento.currentTime = 0;
+        }
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor invalido')
+    }
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
@@ -19,21 +22,21 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;//Template string
-    
+
     tecla.onclick = function () {
-    TocaSom(idAudio);
-   };
+        TocaSom(idAudio);
+    };
 
-   tecla.onkeydown = function (evento) {
-    
-    if (evento.code === 'Space' || evento.code === 'Enter') {
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
     }
-    
-}
 
-   tecla.onkeyup = function(){
-    tecla.classList.remove('ativa');
-   }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
